f81cfc19411e18ef4f6696b96cfecaf57e4cf57d
vishal2468 <vishalpoddar2468@gmail.com>
2023-09-08 09:27:02 +0530

    property-binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,14 +6,15 @@ import { Component } from '@angular/core';
             <h2>
               welcome {{name}}
             </h2>
-            <h2>{{2+2}}</h2>
-            <h2>{{"welcome "+name}}</h2>
-            <h2>{{name.length}}</h2>
-
-            <h2>{{name = "some other name"}}
+            <input [id]="myId" type="text" value= "Vishal">
+            <input id="{{myId}}" type="text" value= "Vishal">
+            <input [disabled]="isDisabled" id="{{myId}}" type="text" value="Vishal">
+            <input [disabled]="false" id="{{myId}}" type="text" value="Vishal">
         `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
+  public myId = "newId"
   public name = "vishal"
+  public isDisabled = false;
 }
