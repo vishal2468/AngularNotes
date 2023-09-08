afd2e5c55113cd43c98eca8b378f9d1f17aa1c17
vishal2468 <vishalpoddar2468@gmail.com>
2023-09-08 08:08:51 +0530

    interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `
+            <h2>
+              welcome {{name}}
+            </h2>
+            <h2>{{2+2}}</h2>
+            <h2>{{"welcome "+name}}</h2>
+            <h2>{{name.length}}</h2>
+
+            <h2>{{name = "some other name"}}
+        `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
-
+  public name = "vishal"
 }
