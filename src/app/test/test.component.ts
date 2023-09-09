fb5a03d252b955a9552176c279aa961bed42abc3
vishal2468 <vishalpoddar2468@gmail.com>
2023-09-09 08:52:03 +0530

    ngFor

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,26 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-            <h2 *ngIf="displayName; else elseBlock1">
-              Test: welcome {{name}}
-            </h2>
-            <ng-template #elseBlock1>
-              <h2>Name is not diaplayed</h2>
-            </ng-template>
-
-            <br>
-
-            <div [ngSwitch]="count">
-              <h2 *ngSwitchCase= "1" > vishal</h2>
-              <h2 *ngSwitchCase= "2" > poddar</h2>
-              <h2 *ngSwitchCase= "3" > hi</h2>
-              <h2 *ngSwitchDefault>Pick again</h2>
+            <div *ngFor="let name of names ; index as i ; first as f ; last as l; odd as o ; even as e">
+              <br>
+              <h1>{{i}} Hi {{name}} {{f}} {{l}}</h1>
             </div>
         `,
   styles: []
 })
 export class TestComponent {
-  public name= "vishal";
-  public count =2 ;
-  public displayName = false;
+  public names = ["Mumbai","Delhi","Kochi","Bangalore","Chennai"]
 }
