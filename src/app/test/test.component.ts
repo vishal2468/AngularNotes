14da61cbe077d8654083b4980a50ba2df4356f66
vishal2468 <vishalpoddar2468@gmail.com>
2023-09-09 07:54:05 +0530

    ngIf

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,19 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-            <h2>
-              welcome {{name}}
+            <h2 *ngIf="displayName; else elseBlock1">
+              Test: welcome {{name}}
             </h2>
-            <input [(ngModel)]="name" type="text">
-            {{name}}
+            <ng-template #elseBlock1>
+              <h2>Name is not diaplayed</h2>
+            </ng-template>
+
+            <br>
+
+            <h2 *ngIf="displayName; then thenBlock2; else elseBlock2;">
+              Test: welcome {{name}}
+            </h2>
+            <ng-template #thenBlock2>
+              <h2>Name is not diaplayed</h2>
+            </ng-template>
+            <ng-template #elseBlock2>
+              <h2>Name is not diaplayed</h2>
+            </ng-template>
         `,
   styles: []
 })
 export class TestComponent {
   public name= "vishal";
-  public greetings= ""
-  logMessage(value:string){
-    console.log(value);
-  }
-  
+  public displayName = false;
 }
